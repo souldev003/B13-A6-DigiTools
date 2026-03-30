ae8fcdaedc685b26918cd6ca6bf62eae89ec488e
souldev003 <mehrabhosen003@gmail.com>
2026-03-30 16:41:05 +0600

    all resources added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
