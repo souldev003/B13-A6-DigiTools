e1c7be818a0baffde9dfef5566e1ce9e8a91b6e9
souldev003 <mehrabhosen003@gmail.com>
2026-03-30 23:53:50 +0600

    navbar responsive issue fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,40 +5,81 @@ const Navbar = () => {
   return (
     <div className="shadow-sm">
       <div className="max-w-300 mx-auto">
-        <div className="navbar bg-base-100 flex items-center justify-between">
-          <div className="">
-            <h1 class="text-3xl p-1 font-bold bg-linear-to-r from-[#7B3FE4] to-[#A020F0] bg-clip-text text-transparent">
+        <div className="navbar bg-white px-4">
+          <div className="navbar-start gap-2">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                ☰
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-white rounded-box z-10 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>Products</a>
+                </li>
+                <li>
+                  <a>Features</a>
+                </li>
+                <li>
+                  <a>Pricing</a>
+                </li>
+                <li>
+                  <a>Testimonials</a>
+                </li>
+                <li>
+                  <a>FAQ</a>
+                </li>
+              </ul>
+            </div>
+
+            <h1 className="cursor-pointer text-3xl font-bold bg-linear-to-r from-[#6030F7] to-[#9415FA] bg-clip-text text-transparent">
               DigiTools
             </h1>
           </div>
 
-          <div className="flex items-center gap-8">
-            <a href="#">
-              <button className="cursor-pointer font-medium">Products</button>
-            </a>
-            <a href="#">
-              <button className="cursor-pointer font-medium">Feature</button>
-            </a>
-            <a href="#">
-              <button className="cursor-pointer font-medium">Pricing</button>
-            </a>
-            <a href="#">
-              <button className="cursor-pointer font-medium">
-                Testimonials
-              </button>
-            </a>
-            <a href="#">
-              <button className="cursor-pointer font-medium">FAQ</button>
-            </a>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="flex gap-8 text-gray-600 font-medium">
+              <li>
+                <a className="cursor-pointer hover:text-black hover:underline hover:decoration-[#6030f7] hover:decoration-2">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer hover:text-black hover:underline hover:decoration-[#6030f7] hover:decoration-2">
+                  Features
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer hover:text-black hover:underline hover:decoration-[#6030f7] hover:decoration-2">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer hover:text-black hover:underline hover:decoration-[#6030f7] hover:decoration-2">
+                  Testimonials
+                </a>
+              </li>
+              <li>
+                <a className="cursor-pointer hover:text-black hover:underline hover:decoration-[#6030f7] hover:decoration-2">
+                  FAQ
+                </a>
+              </li>
+            </ul>
           </div>
 
-          <div className="flex items-center gap-4">
-            <button className="text-lg cursor-pointer btn btn-soft rounded-full">
-              <IoCartOutline></IoCartOutline>
+          <div className="navbar-end gap-4">
+            <IoCartOutline className="text-xl cursor-pointer" />
+
+            <button className="text-gray-600 font-medium hover:text-black cursor-pointer">
+              Login
             </button>
 
-            <button className="btn btn-ghost ">Login</button>
-            <button className="btn btn-primary rounded-full">
+            <button className="px-5 py-2 rounded-full text-white font-medium bg-linear-to-r from-[#6030F7] to-[#9415FA] cursor-pointer">
               Get Started
             </button>
           </div>
